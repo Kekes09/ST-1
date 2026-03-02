e7e7374131db4ba4aa98ce5355f0620a2cf429db
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2026-03-02 09:58:02 +0300

    Fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,56 +3,49 @@
 #include <cmath>
 #include "alg.h"
 
-bool checkPrime(uint64_t znach) {
-  if (znach <= 1) return false;
-  if (znach <= 3) return true;
-  if (znach % 2 == 0 || znach % 3 == 0) return false;
-  
-  for (uint64_t i = 5; i * i <= znach; i += 6) {
-    if (znach % i == 0 || znach % (i + 2) == 0) {
-      return false;
+bool checkPrime(uint64_t value) {
+    if (value <= 1) return false;
+    if (value <= 3) return true;
+    if (value % 2 == 0 || value % 3 == 0) return false;
+    for (uint64_t i = 5; i * i <= value; i += 6) {
+        if (value % i == 0 || value % (i + 2) == 0) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 0) return 0;
-  if (n == 1) return 2;
-  
-  uint64_t count = 1; 
-  uint64_t num = 3;
-  
-  while (count < n) {
-    if (checkPrime(num)) {
-      count++;
+    if (n == 0) return 0;
+    if (n == 1) return 2;
+    uint64_t count = 1;
+    uint64_t candidate = 3;
+    while (count < n) {
+        if (checkPrime(candidate)) {
+            count++;
+        }
+        candidate += 2;
     }
-    num += 2;
-  }
-  
-  return num - 2;  
+    return candidate - 2;
 }
 
-uint64_t nextPrime(uint64_t znach) {
-  uint64_t num = znach + 1;
-  if (num <= 2) return 2;
-  if (num % 2 == 0) num++;
-  while (!checkPrime(num)) {
-    num += 2;  
-  }
-  
-  return num;
+uint64_t nextPrime(uint64_t value) {
+    uint64_t candidate = value + 1;
+    if (candidate <= 2) return 2;
+    if (candidate % 2 == 0) candidate++;
+    while (!checkPrime(candidate)) {
+        candidate += 2;
+    }
+    return candidate;
 }
 
-uint64_t sumPrime(uint64_t gran) {
-  if (gran <= 2) return 0;
-  
-  uint64_t sum = 2;
-  for (uint64_t i = 3; i < gran; i += 2) {
-    if (checkPrime(i)) {
-      sum += i;
+uint64_t sumPrime(uint64_t hbound) {
+    if (hbound <= 2) return 0;
+    uint64_t sum = 2;
+    for (uint64_t i = 3; i < hbound; i += 2) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
     }
-  }
-  
-  return sum;
+    return sum;
 }
