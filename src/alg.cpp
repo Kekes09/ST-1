77a974e6bfad6e9971a6ffc7c17a1340f7da8b9e
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2026-06-01 10:23:32 +0300

    reworked

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,36 @@
-// Copyright 2025 UNN-CS
-#include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+    if (value <= 1) return false;
+    if (value <= 3) return true;
+    if (value % 2 == 0 || value % 3 == 0) return false;
+    for (uint64_t i = 5; i <= value / i; i += 6) {
+        if (value % i == 0 || value % (i + 2) == 0)
+            return false;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+    if (n == 0) return 0;
+    uint64_t counter = 0, numpr = 2;
+    while (true) {
+        if (checkPrime(numpr)) {
+            if (++counter == n) return numpr;
+        }
+        ++numpr;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+    uint64_t numpr = (value < 2) ? 2 : value + 1;
+    while (!checkPrime(numpr)) ++numpr;
+    return numpr;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; ++i)
+        if (checkPrime(i)) sum += i;
+    return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,17 +1,18 @@
-// Copyright 2025 UNN-CS
-
 #include <gtest/gtest.h>
-#include <cstdint>
 #include "alg.h"
 
+TEST(CheckPrimeTest, TwoIsPrime) { EXPECT_TRUE(checkPrime(2)); }
+TEST(CheckPrimeTest, OneIsNotPrime) { EXPECT_FALSE(checkPrime(1)); }
+TEST(CheckPrimeTest, CompositeNumber) { EXPECT_FALSE(checkPrime(15)); }
+TEST(CheckPrimeTest, LargePrime) { EXPECT_TRUE(checkPrime(7919)); }
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
-}
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
-}
+TEST(NPrimeTest, FirstPrime) { EXPECT_EQ(nPrime(1), 2); }
+TEST(NPrimeTest, FifthPrime) { EXPECT_EQ(nPrime(5), 11); }
+TEST(NPrimeTest, TenthPrime) { EXPECT_EQ(nPrime(10), 29); }
+
+TEST(NextPrimeTest, AfterFour) { EXPECT_EQ(nextPrime(4), 5); }
+TEST(NextPrimeTest, AfterPrime) { EXPECT_EQ(nextPrime(11), 13); }
+TEST(NextPrimeTest, FromZero) { EXPECT_EQ(nextPrime(0), 2); }
+
+TEST(SumPrimeTest, SumUpTo10) { EXPECT_EQ(sumPrime(10), 17); }
+TEST(SumPrimeTest, EmptyRange) { EXPECT_EQ(sumPrime(2), 0); }
