942eaf29b5d85e3d2dd7a5f7f4695f4d27bd428f
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2026-06-01 10:46:31 +0300

    fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,13 +6,13 @@ TEST(CheckPrimeTest, Ispr2) { EXPECT_FALSE(checkPrime(4)); }
 TEST(CheckPrimeTest, Ispr3) { EXPECT_FALSE(checkPrime(28)); }
 TEST(CheckPrimeTest, Ispr4) { EXPECT_TRUE(checkPrime(97)); }
 
-TEST(NPrimeTest, NOfpr1) { EXPECT_EQ(nPrime(10), 11); }
+TEST(NPrimeTest, NOfpr1) { EXPECT_EQ(nPrime(1), 2); }
 TEST(NPrimeTest, NOfpr2) { EXPECT_EQ(nPrime(13), 17); }
-TEST(NPrimeTest, NOfpr3) { EXPECT_EQ(nPrime(15), 47); }
+TEST(NPrimeTest, NOfpr3) { EXPECT_EQ(nPrime(7), 17); }
 
-TEST(NextPrimeTest, nextpr1) { EXPECT_EQ(nextPrime(4), 5); }
-TEST(NextPrimeTest, nextpr2) { EXPECT_EQ(nextPrime(11), 13); }
-TEST(NextPrimeTest, nextpr3) { EXPECT_EQ(nextPrime(0), 2); }
+TEST(NextPrimeTest, nextpr1) { EXPECT_EQ(nextPrime(10), 11); }
+TEST(NextPrimeTest, nextpr2) { EXPECT_EQ(nextPrime(13), 17); }
+TEST(NextPrimeTest, nextpr3) { EXPECT_EQ(nextPrime(100), 101); }
 
 TEST(SumPrimeTest, sum1) { EXPECT_EQ(sumPrime(20), 77); }
-TEST(SumPrimeTest, sum2) { EXPECT_EQ(sumPrime(2), 0); }
+TEST(SumPrimeTest, sum2) { EXPECT_EQ(sumPrime(3), 2); }
